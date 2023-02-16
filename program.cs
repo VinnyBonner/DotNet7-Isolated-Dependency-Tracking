2d9d8d2a84149f10ab5c422d0f8feaa28c8b036a
Vinny <92878154+VinnyBonner@users.noreply.github.com>
2023-02-16 13:00:42 -0800

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,7 +1,19 @@
+//using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
+
+/* Tested with the below as well, and still same behavior. 
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(builder =>
+    {
+        builder
+            .AddApplicationInsights()
+            .AddApplicationInsightsLogger();
+    })
+    .Build();
+*/    
 
 host.Run();
