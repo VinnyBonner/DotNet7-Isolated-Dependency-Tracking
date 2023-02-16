11436dd7e270f42faf422e60c33b6c56bdee7104
Vinny <92878154+VinnyBonner@users.noreply.github.com>
2023-02-16 13:08:22 -0800

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,7 +5,7 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .Build();
 
-/* Tested with the below as well, and still same behavior. 
+/* Tested with the below as well per https://github.com/Azure/azure-functions-dotnet-worker/pull/944, and still same behavior. 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults(builder =>
     {
